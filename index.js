02633ec2b866fde1adb27b6a38c46d424d985dbd
mdrummond1 <mattatatter@gmail.com>
2020-05-09 23:55:54 -0500

    fixed mose of fillTables. albums still not displaying

--- index.js
+++ index.js
@@ -12,63 +12,101 @@ const changeElem = function(id){
     
 }
 
-//add content to a table
-const fillTable = function(type, data){
-    
-    //books displayed like this: <ISBN> <Title> <Author> <Due Date>
-    //movies displayed like this: <Title> <Year> <Genre> <Star> <Due Date>
-    //albums displayed like this: <Title> <Artist> <Genre> <Release Date> <Due Date> 
-    
-    if (type === "all"){
-        //clear all tables except for headings
-        var table = document.getElementById("books");    
-        while (table.childNodes.length > 5){
-            table.removeChild(table.lastChild);
-        }    
-
-        var table = document.getElementById("movies");
-        while (table.childNodes.length > 5){
-            table.removeChild(table.lastChild);
-        }
-
-        var table = document.getElementById("albums");
-        while (table.childNodes.length > 5){
-            table.removeChild(table.lastChild);
-        }
+const clearTables = function(){
+    //clear all tables except for headings
+    var table = document.getElementById("books");    
+    while (table.childNodes.length > 5){
+         table.removeChild(table.lastChild);
     }    
-    
-    
-    //fill table with data
-    if (type === "all"){
-        //TODO: add functionality for filling all tables
-        
-    }else if (type === "books"){
-    
-        var numResults = data.resultCount;
-        var newRow, newCell, newData;
-        for (let i = 0; i < numResults; i++){
-        
-            newRow = document.createElement("TR");//create new row
-            newCell = document.createElement("TD");//create new cell
-            newCell.innerHTML = data.title[i];//add text to first cell
-            newRow.appendChild(newCell);//add the new cell to the row
 
-            //repeat all except new row for the rest of the columns
-            newCell.innerHTML = data.column_1[i];
-            newRow.appendChild(newCell);
-
-            newCell.innerHTML = data.column_2[i];
-            newRow.appendChild(newCell);
-
-            newCell.innerHTML = data.column_3[i];
-            newRow.appendChild(newCell);
-            node.appendChild(textnode);
-            searchBy.appendChild(node);
-        }
-    }else if (type === "movies"){
-
+    var table = document.getElementById("movies");
+    while (table.childNodes.length > 5){
+        table.removeChild(table.lastChild);
     }
+
+    var table = document.getElementById("albums");
+    while (table.childNodes.length > 5){
+        table.removeChild(table.lastChild);
+    }
+}
+
+
+//add content to a table
+const fillTables = function(data){
+    
+    clearTables();
+    
+    //books displayed like this: <Title> <Author> <ISBN>
+    for (let i = 0; i < data.book_count; i++){
         
+        newRow = document.createElement("TR");//create new row
+        newCell = document.createElement("TD");//create new cell
+        
+        newCell.innerHTML = data.book_title[i];//add text to cell
+        newRow.appendChild(newCell);//add the new cell to the row
+        
+        //repeat all except new row for the rest of the columns
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.book_author[i];
+        newRow.appendChild(newCell);
+        
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.book_isbn[i];
+        newRow.appendChild(newCell);
+        
+        document.getElementById("books").appendChild(newRow);//add new row to table
+    }
+    
+    //movies displayed like this: <Title> <Year> <Star> <Genre>
+    for (let i = 0; i < data.movie_count; i++){
+        
+        newRow = document.createElement("TR");//create new row
+        newCell = document.createElement("TD");//create new cell
+        
+        newCell.innerHTML = data.movie_title[i];//add text to cell
+        newRow.appendChild(newCell);//add the new cell to the row
+        
+        //repeat all except new row for the rest of the columns
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.movie_year[i];
+        newRow.appendChild(newCell);
+        
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.movie_star[i];
+        newRow.appendChild(newCell);
+        
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.movie_genre[i];
+        newRow.appendChild(newCell);
+        document.getElementById("movies").appendChild(newRow);//add new row to table
+    }
+    
+    
+    //albums displayed like this: <Title> <Artist> <Release Date> <Genre>
+    for (let i = 0; i < data.albums_count; i++){
+        //TODO: find out why the albums aren't displaying correctly
+        
+        newRow = document.createElement("TR");//create new row
+        newCell = document.createElement("TD");//create new cell
+        
+        newCell.innerHTML = data.album_title[i];//add text to cell
+        newRow.appendChild(newCell);//add the new cell to the row
+
+        //repeat all except new row for the rest of the columns
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.album_artist[i];
+        newRow.appendChild(newCell);
+
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.album_release[i];
+        newRow.appendChild(newCell);
+
+        newCell = document.createElement("TD");//create new cell
+        newCell.innerHTML = data.album_genre[i];
+        newRow.appendChild(newCell);
+ 
+        document.getElementById("albums").appendChild(newRow);//add new row to table
+    }
 }
 
 const populateSearchBy = function(){
@@ -160,14 +198,20 @@ const populateSearchBy = function(){
 const sendSearch = function(){
     var xhr = new XMLHttpRequest();
     // the /search is the url you're getting from, relative to the site
-    var url = "https://jsonplaceholder.typicode.com/posts";
     
-    xhr.open("GET", url, true);
+    
+    xhr.open("GET", "/search", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.responseType = "json";
     
     xhr.send(JSON.stringify({
-        //TODO: put values here from the query: searchFor, searchBy, searchText;
+        //TODO: put values here from the query: searchFor, searchBy, searchText
+        searchFor: "books",
+        searchBy: "title",
+        searchText: "Lord of the Rings"
+        /* searchFor : document.getElementById("searchFor").value,
+        searchBy : document.getElementById("searchBy").value,
+        searchText : document.getElementById("results").tex */
     }));
 
     xhr.onreadystatechange = function(){
@@ -175,28 +219,31 @@ const sendSearch = function(){
         //readyState 4 means that the request is done
         if(xhr.readyState == 4){
             //deal with the data according to what was searched for
-            var searchingFor = document.getElementById("searchFOR").value;
+            var searchingFor = document.getElementById("searchFor").value;
             var json = xhr.response;
 
-            switch (searchingFor){
-                case "books":
-                    fillTable("books", json);
-                    break;
-                case "movies":
-                    fillTable("movies", json);
-                    break;
-                case "albums":
-                    fillTable("albums", json);
-                    break;
-                case "all":
-                    fillTable("all", json);
-                    break;
-                case "card":
-                    fillTable("all", json);
-                    break;
-            }
+            fillTables(json);
             console.log(json);
         }
     };
+}
+
+//test data for fillTables()
+var test = {
+    book_count: 1,
+    book_isbn:[31452950],
+    book_title:["The Lord of the Rings"],
+    book_author:["JRR Tolkien"],
+    book_genre:["Fantasy"],
+    movie_count:1,
+    movie_title:["The Lord of the Rings"],
+    movie_year:[2001],
+    movie_star:["Ian McKellan"],
+    movie_genre:["Fantasy"],
+    album_count:3,
+    album_title:["Fellowship of the Ring","The Two Towers","The Return of the King"],
+    album_release:[2001,2002,2003],
+    album_count:["Howard Shore","Howard Shore","Howard Shore"],
+    album_genre:["soundtrack","soundtrack","soundtrack"]
 }
 
