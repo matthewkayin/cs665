046bd65e8d998057e4c4fc6132e0d9bb580b95f3
matthewkayin <matthewkayin@gmail.com>
2020-05-10 19:53:09 -0500

    Added more database values
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

--- project.sql
+++ project.sql
@@ -19,7 +19,7 @@ CREATE TABLE books(
     isbn VARCHAR(13) NOT NULL,
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
-    due_date DATE NOT NULL,
+    due_date DATE,
     PRIMARY KEY (copy_number),
     FOREIGN KEY(card_number) REFERENCES customers(card_number)
 );
@@ -32,7 +32,7 @@ CREATE TABLE movies(
     year SMALLINT NOT NULL,
     genre VARCHAR(255) NOT NULL,
     star VARCHAR(255) NOT NULL,
-    due_date DATE NOT NULL,
+    due_date DATE,
     PRIMARY KEY (copy_number),
     FOREIGN KEY(card_number) REFERENCES customers(card_number)
 );
@@ -45,25 +45,30 @@ CREATE TABLE albums(
     artist VARCHAR(255) NOT NULL,
     genre VARCHAR(255) NOT NULL,
     release_date DATE NOT NULL,
-    due_date DATE NOT NULL,
+    due_date DATE,
     PRIMARY KEY (copy_number),
     FOREIGN KEY(card_number) REFERENCES customers(card_number)
 );
 
-INSERT INTO books (isbn, title, author, due_date)
-            VALUES ("0000000000", "Jane Eyre", "Charlotte Bronte", CURDATE()),
-                   ("0000000001", "The Lord of the Rings", "JRR Tolkein", CURDATE()),
-                   ("0000000002", "The Remains of the Day", "Kazuo Ishiguro", CURDATE()),
-                   ("0000000003", "Dune", "Frank Herbert", CURDATE());
+INSERT INTO customers (first_name, last_name, email, address)
+            VALUES ("Matt", "Madden", "matt@madden.com", "1023 S Other St."),
+                   ("Matt", "Drummond", "matt@drummond.com", "2050 W Main St."),
+                   ("Angel", "Lopez", "angel@lopez.com", "3033 E Sesame St.");
 
-INSERT INTO movies (title, year, star, genre, due_date)
-            VALUES ("Star Wars", 1975, "Mark Hammill", "Sci-Fi", CURDATE()),
-                   ("The Social Network", 2010, "Jesse Eisenberg", "Drama", CURDATE()),
-                   ("Parasite", 2019, "Song Kang-ho", "Drama", CURDATE()),
-                   ("Knives Out", 2019, "Ana de Armas", "Mystery", CURDATE());
+INSERT INTO books (isbn, title, author, due_date, card_number)
+            VALUES ("0000000000", "Jane Eyre", "Charlotte Bronte", STR_TO_DATE("5/22/2020", "%m/%d/%Y"), 1),
+                   ("0000000001", "The Lord of the Rings", "JRR Tolkein", NULL, NULL),
+                   ("0000000002", "The Lies of Locke Lamora", "Scott Lynch", NULL, NULL),
+                   ("0000000003", "Harry Potter and the Half Blood Prince", "JK Rowling", NULL, NULL),
+                   ("0000000004", "Pride and Prejudice", "Jane Austen", NULL, NULL),
+                   ("0000000005", "Fahrenheit 451", "Ray Bradbury", NULL, NULL),
+                   ("0000000006", "The Girl With the Dragon Tattoo", "Steig Larson", NULL, NULL),
+                   ("0000000007", "The Name of the Wind", "Patrick Rothfuss", NULL, NULL),
+                   ("0000000008", "The Remains of the Day", "Kazuo Ishiguro", NULL, NULL),
+                   ("0000000009", "Dune", "Frank Herbert", NULL, NULL);
 
-INSERT INTO albums (title, artist, genre, release_date, due_date)
-            VALUES ("Petals for Armor", "Hayley Williams", "Pop", CURDATE(), CURDATE()),
-                   ("With Teeth", "Nine Inch Nails", "Rock", CURDATE(), CURDATE()),
-                   ("Melodrama", "Lorde", "Pop", CURDATE(), CURDATE()),
-                   ("American Football", "American Football", "Indie", CURDATE(), CURDATE());
+INSERT INTO movies (title, year, star, genre, due_date, card_number)
+            VALUES ("Parasite", 2019, "Song Kang-ho", "Drama", NULL, NULL);
+
+INSERT INTO albums (title, artist, genre, release_date, due_date, card_number)
+            VALUES ("Petals for Armor", "Hayley Williams", "Pop", NULL, NULL);
